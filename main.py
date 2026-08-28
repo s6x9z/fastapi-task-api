45cb8733e24a369f5e3e8013c8ab643f2d1ba5da
Sadeq <sadeqsleiman9@gmail.com>
2026-08-28 21:36:48 +0300

    Satge 2: add in memory tasks list and create endpoints

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+tasks_db = []
+
+class TaskCreate(BaseModel):
+    title: str
+    completed: bool = False
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    completed: bool
+
 
 @app.get("/")
 def get_root():
@@ -12,6 +26,23 @@ def get_root():
 
 @app.get("/health")
 def get_health():
-    return {
-        "status": "ok"
+    return {"status": "ok"}
+
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": tasks_db}
+
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+def create_task(task: TaskCreate):
+    new_id = len(tasks_db) + 1
+    
+    new_task = {
+        "id": new_id,
+        "title": task.title,
+        "completed": task.completed
     }
+
+    tasks_db.append(new_task)
+    
+    return new_task
