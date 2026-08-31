e9122dcb2642032b2276cb56804923e59814d9f4
Sadeq <sadeqsleiman9@gmail.com>
2026-08-31 20:49:29 +0300

    Stage 2: Add SQLAlchemy Task model and Alembic migrations

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    done = Column(Boolean, default=False, nullable=False)
